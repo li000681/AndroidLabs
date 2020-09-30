19e5198e4466dfd44ffebf9a4cc105170afde027
li000681 <li000681@algonquinlive.com>
2020-09-30 18:12:43 -0400

    step 1

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.androidlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_linear);
+        //setContentView(R.layout.activity_main_grid);
+        //setContentView(R.layout.activity_main_relative);
+        TextView tx= findViewById(R.id.TextView);
+        Button bt =findViewById(R.id.button);
+        bt.setOnClickListener( v -> Toast.makeText(MainActivity.this, getResources().getString(R.string.toast_message) , Toast.LENGTH_LONG).show());
+
+
+        CheckBox checkBox=findViewById(R.id.checkbox);
+
+        ImageButton ib= findViewById(R.id.imageButton);
+        Switch sw = findViewById(R.id.switch1);
+        sw.setOnCheckedChangeListener((cb,b)->{if(b)
+        {Snackbar.make(sw,"The switch is now on",Snackbar.LENGTH_LONG).setAction("Undo", click -> sw.setChecked(!b)).show();}else{
+            Snackbar.make(sw,"The switch is now off",Snackbar.LENGTH_LONG).setAction("Undo", click -> sw.setChecked(!b)).show();
+        }});
+
+
     }
 }
